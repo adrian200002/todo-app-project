1b783f262d0ee53da8262b2d35290f6430d66827
Adrian <adrian.krause.ak@gmail.com>
2024-01-09 11:52:30 +0100

    add project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useEffect } from 'react';
+import { UserWarning } from './UserWarning';
+import { getTodos } from './api/todos';
+import { TodoList } from './components/TodoList';
+import { useTodoContext } from './context';
+import { Errors } from './types/Errors';
+import { Header } from './components/Header';
+import { ErrorAlert } from './components/ErrorAlert';
 
-function App() {
+export const App: React.FC = () => {
+  const {
+    setAllTodos,
+    errorHandler,
+    USER_ID,
+  } = useTodoContext();
+
+  useEffect(() => {
+    const loadTodos = async () => {
+      try {
+        const todos = await getTodos(USER_ID);
+
+        setAllTodos(todos);
+      } catch (error) {
+        errorHandler(Errors.loadError);
+      }
+    };
+
+    loadTodos();
+  }, [setAllTodos, errorHandler, USER_ID]);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+      <div className="todoapp__content">
+        <Header />
+        <section className="todoapp__main" data-cy="TodoList">
+          <TodoList />
+        </section>
+      </div>
+      <ErrorAlert />
     </div>
   );
-}
-
-export default App;
+};
